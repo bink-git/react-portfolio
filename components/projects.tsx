2caeb56251f426c33174b7002cd0187b7d7e1f76
BINK <bink@email.ua>
2023-10-17 18:42:00 +0300

    add skills section

--- components/projects.tsx
+++ components/projects.tsx
@@ -6,10 +6,10 @@ import Project from "./project";
 import { useSectionInView } from "@/lib/hooks";
 
 const Projects = () => {
-  const { ref } = useSectionInView("Home", 0.5);
+  const { ref } = useSectionInView("Projects", 0.5);
 
   return (
-    <section ref={ref} id="projects" className="scroll-mt-28">
+    <section ref={ref} id="projects" className="mb-28 scroll-mt-28">
       <SectionHeading>My Projects</SectionHeading>
       <div>
         {projectsData.map((project) => (
--- components/skills.tsx
+++ components/skills.tsx
@@ -1,10 +1,49 @@
+"use client";
+
 import React from "react";
 import SectionHeading from "./section-heading";
+import { skillsData } from "@/lib/data";
+import { useSectionInView } from "@/lib/hooks";
+import { motion } from "framer-motion";
+
+const variants = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.05 * index,
+    },
+  }),
+};
 
 const Skills = () => {
+  const { ref } = useSectionInView("Skills");
+
   return (
-    <section>
+    <section
+      ref={ref}
+      className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40"
+    >
       <SectionHeading>My Skills</SectionHeading>
+      <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
+        {skillsData.map((skill, index) => (
+          <motion.li
+            className="rounded-2xl border border-black/[0.1] bg-white px-5 py-2"
+            key={index}
+            variants={variants}
+            initial="initial"
+            whileInView="animate"
+            viewport={{ once: true }}
+            custom={index}
+          >
+            {skill}
+          </motion.li>
+        ))}
+      </ul>
     </section>
   );
 };
